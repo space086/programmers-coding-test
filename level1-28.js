5bfcec6414bffb96b7be8743d77ff489b790ced9
space086 <space_086@naver.com>
2022-03-31 22:42:31 +0900

    약수의 합

---
+++ level1-28.js
@@ -0,0 +1,11 @@
+// javascript coding test - 약수의 합 (lv1)
+function solution(n) {
+  var answer = [];
+  for(let i = 0; i <=n ; i++){
+      if(n%i ===0){
+         answer.push(i);
+      }
+  }
+  var sum = answer.reduce((a,b)=> a + b,0);
+  return sum;
+}
